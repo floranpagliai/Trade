ba6f79596d2aaacba27ddb20185728ec36bd510c
shked0wn <floran.pagliai@gmail.com>
2013-01-31 18:14:44 +0100

    update

---
+++ src/Logger.cpp
@@ -0,0 +1,26 @@
+//
+// Logger.cpp for Logger in /home/paglia_f//Work/ProgElemApp/Trade/src
+// 
+// Made by floran pagliai
+// Login   <paglia_f@epitech.net>
+// 
+// Started on  Thu Jan 31 17:07:27 2013 floran pagliai
+// Last update Thu Jan 31 18:14:13 2013 floran pagliai
+//
+
+#include "Logger.h"
+
+Logger::Logger(std::string fileName) {
+  _fileName = fileName;
+}
+
+Logger::~Logger() {
+}
+
+void	Logger::writeLog(std::string const &log) const {
+  std::ofstream	logFile;
+
+  logFile.open(_fileName.c_str(), std::ios::out);
+  logFile << log << std::endl;
+  logFile.close();
+}
---
+++ src/Logger.h
@@ -0,0 +1,28 @@
+/*
+** Logger.h for Logger in /home/paglia_f//Work/ProgElemApp/Trade/src
+** 
+** Made by floran pagliai
+** Login   <paglia_f@epitech.net>
+** 
+** Started on  Thu Jan 31 17:00:26 2013 floran pagliai
+** Last update Thu Jan 31 17:16:28 2013 floran pagliai
+*/
+
+#ifndef __LOGGER_H_
+#define __LOGGER_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Logger {
+ private:
+  std::string	_fileName;
+
+ public:
+  Logger(std::string fileName);
+  ~Logger();
+
+  void		writeLog(std::string const &) const;
+};
+#endif
