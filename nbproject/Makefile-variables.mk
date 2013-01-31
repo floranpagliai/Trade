08eaab2eb37490e490cfd29dedebefea23dc1b30
Floran <floran.pagliai@epitech.eu>
2013-01-31 03:22:19 +0100

    First commit Signed-off-by:Floran <floran.pagliai@epitech.eu>

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=None-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-MacOSX
+CND_ARTIFACT_NAME_Debug=libtrade.a
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-MacOSX/libtrade.a
+CND_PACKAGE_DIR_Debug=dist/Debug/None-MacOSX/package
+CND_PACKAGE_NAME_Debug=Trade.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-MacOSX/package/Trade.tar
+# Release configuration
+CND_PLATFORM_Release=None-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/None-MacOSX
+CND_ARTIFACT_NAME_Release=libtrade.a
+CND_ARTIFACT_PATH_Release=dist/Release/None-MacOSX/libtrade.a
+CND_PACKAGE_DIR_Release=dist/Release/None-MacOSX/package
+CND_PACKAGE_NAME_Release=Trade.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-MacOSX/package/Trade.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
